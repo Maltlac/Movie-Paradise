0c454ec6ec2d12579ffc63453d1a46e24c788911
Maltlac <maltlac27@gmail.com>
2025-03-03 13:40:00 +0100

    test

--- movieParadise-app/tests/Feature/ExampleTest.php
+++ movieParadise-app/tests/Feature/ExampleTest.php
@@ -10,10 +10,31 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function testLogin(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    public function testRegister(): void
+    {
+        $response = $this->get('/register');
+
+        $response->assertStatus(200);
+    }
+
+    public function testForgotPassWord(): void
+    {
+        $response = $this->get('/forget-password');
+
+        $response->assertStatus(200);
+    }
+
+    public function testUnauthentified()
+    {
+        $response = $this->get("/"); //On se connecte à la racine du site
+        $response->assertStatus(302); // On est redirigé
+        $response->assertRedirect("/login"); // On est bien redirigé vers la route de connexion
     }
 }
